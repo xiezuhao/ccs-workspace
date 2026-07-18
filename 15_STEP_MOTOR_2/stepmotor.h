269b2e7f3fe4570b3e05ed42dfcc64bc70c2d6b3
xiezuhao <3217391607@qq.com>
2026-07-18 20:58:59 +0800

    fix: use 0.9-degree motor step angle

--- 15_STEP_MOTOR_2/stepmotor.h
+++ 15_STEP_MOTOR_2/stepmotor.h
@@ -9,12 +9,13 @@
 // PA15 SLP
 // PA16 RST
 
+// 14H28HM-0404A2为0.9度整步电机，每圈400步
 // DRV8825的M0/M1/M2未置高，当前硬件为整步模式
-// 一脉冲1.8度，脉冲频率 = 角速度 / 1.8度
+// 一脉冲0.9度，脉冲频率 = 角速度 / 0.9度
 
 #include "ti_msp_dl_config.h"
 
-#define STEPMOTOR_STEP_ANGLE_DEG (1.8f)
+#define STEPMOTOR_STEP_ANGLE_DEG (0.9f)
 
 void stepmotor_init(void);
 void stepmotor_dir_set(uint8_t direction, uint8_t stepper_id);
