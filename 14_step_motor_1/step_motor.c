7149d2bf91bea379fbf419f68e593ce2cdcb90c8
xiezuhao <3217391607@qq.com>
2026-07-20 12:39:22 +0800

    fix CCS diagnostics and back up motor documentation

--- 14_step_motor_1/step_motor.c
+++ 14_step_motor_1/step_motor.c
@@ -1,11 +1,11 @@
 #include"step_motor.h"
 
 void step_motor_init(void){
-    DL_GPIO_setPins(STEP_MOTOR_PORT,STEP_MOTOR_PWM2_PIN);
-    DL_GPIO_setPins(STEP_MOTOR_PORT,STEP_MOTOR_RST2_PIN);
-    DL_GPIO_setPins(STEP_MOTOR_PORT,STEP_MOTOR_SLP2_PIN);
-    DL_GPIO_setPins(STEP_MOTOR_PORT,STEP_MOTOR_DIR2_PIN);
-    DL_GPIO_setPins(STEP_MOTOR_PORT,STEP_MOTOR_DCY2_PIN);
+    DL_GPIO_setPins(step_motor_PORT,step_motor_PWM2_PIN);
+    DL_GPIO_setPins(step_motor_PORT,step_motor_RST2_PIN);
+    DL_GPIO_setPins(step_motor_PORT,step_motor_SLP2_PIN);
+    DL_GPIO_setPins(step_motor_PORT,step_motor_DIR2_PIN);
+    DL_GPIO_setPins(step_motor_PORT,step_motor_DCY2_PIN);
 }
 
 //方向控制
@@ -13,10 +13,10 @@ void step_motor_dir_set(uint8_t dirction, uint8_t stepper_id)
 {
 if(stepper_id == 2){
     if(dirction == 0){
- DL_GPIO_clearPins(STEP_MOTOR_PORT,STEP_MOTOR_DIR2_PIN);
+ DL_GPIO_clearPins(step_motor_PORT,step_motor_DIR2_PIN);
     }
     else{
-         DL_GPIO_setPins(STEP_MOTOR_PORT,STEP_MOTOR_DIR2_PIN);
+         DL_GPIO_setPins(step_motor_PORT,step_motor_DIR2_PIN);
     
         }
     }
@@ -27,10 +27,10 @@ void step_motor_step_set(uint8_t step, uint8_t stepper_id)
 {
 if(stepper_id == 2){
     if(step == 0){
- DL_GPIO_clearPins(STEP_MOTOR_PORT,STEP_MOTOR_DIR2_PIN);
+ DL_GPIO_clearPins(step_motor_PORT,step_motor_DIR2_PIN);
     }
     else{
-         DL_GPIO_setPins(STEP_MOTOR_PORT,STEP_MOTOR_DIR2_PIN);
+         DL_GPIO_setPins(step_motor_PORT,step_motor_DIR2_PIN);
 
         }
     }
